6f8962f18d8c1dadc9fdf3192d4e2eed5b2f4f6d
david barth <dzb10035@yahoo.com>
2020-06-02 23:44:58 +0200

    Start of modal window

--- active/src/Components/Portfolio/ModalSkills.js
+++ active/src/Components/Portfolio/ModalSkills.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import {Layer} from 'grommet'; 
+import {Box, Heading, Markdown, Layer, Grid} from 'grommet'; 
 
 
 const ModalSkills = (props) => {
@@ -8,9 +8,29 @@ const ModalSkills = (props) => {
         <Layer  onEsc={props.popUp}
                 onClickOutside={props.popUp}        
                         >
-            <div>
-                <h1>hi</h1>
-            </div>
+            <Grid
+                rows={['medium', 'small']}
+                columns={['medium', 'small', 'medium']}
+                areas={[
+                    {name: "wordSummary", start: [0, 0], end: [1, 1]}, 
+                    {name: "techSummary", start: [2, 0], end: [2, 1]}, 
+                ]}
+                >
+                
+                <Box gridArea="wordSummary" color="brand" border="all">
+                    <Heading color="brand" level="2">
+                        The Details
+                    </Heading>
+                    <Markdown color="brand">
+                        Combining **machine learning** and **REST APIs**
+                    </Markdown>
+                </Box>
+
+                <Box gridArea="techSummary" border="all">
+                    <h1>hi</h1>
+                </Box>
+
+            </Grid>
         </Layer>
     )
 
