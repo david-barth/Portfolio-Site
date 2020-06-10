646643a80ad9ec0ce622f68bab6eda92a938e650
david barth <dzb10035@yahoo.com>
2020-06-11 00:01:00 +0200

    Further work on modal window

--- active/src/Components/Portfolio/ModalSkills.js
+++ active/src/Components/Portfolio/ModalSkills.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import {Box, Heading, Markdown, Layer, Grid} from 'grommet'; 
+import {Box, Button, Heading, Layer, List, Grid} from 'grommet'; 
 
 
 const ModalSkills = (props) => {
@@ -17,17 +17,44 @@ const ModalSkills = (props) => {
                 ]}
                 >
                 
-                <Box gridArea="wordSummary" color="brand" border="all">
-                    <Heading color="brand" level="2">
+                <Box gridArea="wordSummary" color="brand" border="all" >
+                    <Heading color="brand" level="2" alignSelf="center">
                         The Details
                     </Heading>
-                    <Markdown color="brand">
-                        Combining **machine learning** and **REST APIs**
-                    </Markdown>
+                    <List
+                        primaryKey="point"
+                        secondaryKey="explanation"
+                        pad="small"
+                        margin={{left: "medium", right: "medium"}}
+                        background={["brand", "brand", "brand", "brand"]}
+                        data={[
+                            { point: 'RESTFUL:', explanation: "The web app uses 2 REST APIs: Twitter and The Guardian News" },
+                            { point: 'Machine Learning:', explanation: "A trained neural network is integrated serverside, based on the powerful Tensorflow API (JS implementation)." },
+                            { point: 'Supervised Training', explanation: "The neural network is fully customized and self-trained using 80000+ scrapped tweets, gathered with custom tools" },
+                            { point: 'React Powered', explanation: "The frontend is powered by ReactJS and styling is based on the popular Materialize UI framework"},
+                        ]}
+                    />
+                    <Button 
+                            default 
+                            margin="small" 
+                            label="Live Demo" 
+                            href="https://tweetrec.herokuapp.com/"
+                            alignSelf="center"
+                            />
+
                 </Box>
 
                 <Box gridArea="techSummary" border="all">
-                    <h1>hi</h1>
+                    <Box direction="row">
+                        <Button primary label="React" />
+                        <Button primary label="TensorFlowJS" />
+                        <Button primary label="REST API" />
+                    </Box>
+                    <Box direction="row">
+                        <Button primary label="Materialize UI" />
+                        <Button primary label="ExpressJS" />
+                        <Button primary label="NodeJS" />
+                    </Box>
                 </Box>
 
             </Grid>
