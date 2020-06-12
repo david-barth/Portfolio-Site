a467f33331001c7db082e793bf6bcfa828a5e7fb
david barth <dzb10035@yahoo.com>
2020-06-12 23:50:30 +0200

    Modal Skills Rework

---
+++ active/src/Components/Portfolio/BackUpComponents/ModalSkills.js
@@ -0,0 +1,58 @@
+import React from 'react'; 
+import {Box, Button, Heading, Layer, List, Grid} from 'grommet'; 
+
+//Component Imports: 
+import TechSummary from './TechSummary'; 
+ 
+
+const ModalSkills = (props) => {
+
+    return (
+        <Layer  onEsc={props.popUp}
+                onClickOutside={props.popUp}        
+                        >
+            <Grid
+                rows={['medium', 'small']}
+                columns={['medium', 'small', 'medium']}
+                areas={[
+                    {name: "wordSummary", start: [0, 0], end: [1, 1]}, 
+                    {name: "techSummary", start: [2, 0], end: [2, 1]}, 
+                ]}
+                >
+                
+                <Box gridArea="wordSummary" color="brand" border="all" >
+                    <Heading color="brand" level="2" alignSelf="center">
+                        The Details
+                    </Heading>
+                    <List
+                        primaryKey="point"
+                        secondaryKey="explanation"
+                        pad="small"
+                        margin={{left: "medium", right: "medium"}}
+                        background={["brand", "brand", "brand", "brand"]}
+                        data={[
+                            { point: 'RESTFUL:', explanation: "The web app uses 2 REST APIs: Twitter and The Guardian News" },
+                            { point: 'Machine Learning:', explanation: "A trained neural network is integrated serverside, based on the powerful Tensorflow API (JS implementation)." },
+                            { point: 'Supervised Training', explanation: "The neural network is fully customized and self-trained using 80000+ scrapped tweets, gathered with custom tools" },
+                            { point: 'React Powered', explanation: "The frontend is powered by ReactJS and styling is based on the popular Materialize UI framework"},
+                        ]}
+                    />
+                    <Button 
+                            default 
+                            margin="small" 
+                            label="Live Demo" 
+                            href="https://tweetrec.herokuapp.com/"
+                            alignSelf="center"
+                            />
+
+                </Box>
+
+                <TechSummary /> 
+
+            </Grid>
+        </Layer>
+    )
+
+}
+
+export default ModalSkills; 
--- active/src/Components/Portfolio/ModalSkills.js
+++ active/src/Components/Portfolio/ModalSkills.js
@@ -1,8 +1,6 @@
 import React from 'react'; 
-import {Box, Button, Heading, Layer, List, Grid} from 'grommet'; 
+import {Box, Button, Heading, Layer, Grid, Text} from 'grommet'; 
 
-//Component Imports: 
-import TechSummary from './TechSummary'; 
  
 
 const ModalSkills = (props) => {
@@ -10,46 +8,99 @@ const ModalSkills = (props) => {
     return (
         <Layer  onEsc={props.popUp}
                 onClickOutside={props.popUp}        
-                        >
-            <Grid
-                rows={['medium', 'small']}
-                columns={['medium', 'small', 'medium']}
+        >
+
+        <Grid
+                rows={["xsmall", "small", "small", "xsmall"]}
+                columns={["medium", "medium", "medium"]}
                 areas={[
-                    {name: "wordSummary", start: [0, 0], end: [1, 1]}, 
-                    {name: "techSummary", start: [2, 0], end: [2, 1]}, 
+                    {name: "Title", start: [0, 0], end: [2, 0]},
+                    {name: "Bullet1", start: [0, 1], end: [0, 1]},
+                    {name: "Bullet2", start: [1, 1], end: [1, 1]},
+                    {name: "Bullet3", start: [0, 2], end: [0, 2]},
+                    {name: "Bullet4", start: [1, 2], end: [1, 2]},
+                    {name: "SeeMoreBtn", start: [0, 3], end: [1, 3]},
+                    {name: "TechList", start: [2, 1], end: [2, 3]}
                 ]}
-                >
-                
-                <Box gridArea="wordSummary" color="brand" border="all" >
-                    <Heading color="brand" level="2" alignSelf="center">
-                        The Details
+        >
+
+            <Box gridArea="Title">
+                <Heading level="2" textAlign="center" color="brand">
+                    #The Details
+                </Heading>
+            </Box>
+
+            <Box gridArea="Bullet1">
+                <Box pad={{left: "medium", right: "large"}}>
+                    <Heading level="3" textAlign="start" color="brand">
+                        .RESTful: 
                     </Heading>
-                    <List
-                        primaryKey="point"
-                        secondaryKey="explanation"
-                        pad="small"
-                        margin={{left: "medium", right: "medium"}}
-                        background={["brand", "brand", "brand", "brand"]}
-                        data={[
-                            { point: 'RESTFUL:', explanation: "The web app uses 2 REST APIs: Twitter and The Guardian News" },
-                            { point: 'Machine Learning:', explanation: "A trained neural network is integrated serverside, based on the powerful Tensorflow API (JS implementation)." },
-                            { point: 'Supervised Training', explanation: "The neural network is fully customized and self-trained using 80000+ scrapped tweets, gathered with custom tools" },
-                            { point: 'React Powered', explanation: "The frontend is powered by ReactJS and styling is based on the popular Materialize UI framework"},
-                        ]}
-                    />
-                    <Button 
-                            default 
-                            margin="small" 
-                            label="Live Demo" 
-                            href="https://tweetrec.herokuapp.com/"
-                            alignSelf="center"
-                            />
-
+                    <Text color="brand" textAlign="start"> 
+                        -The web app uses 2 REST APIs: Twitter and The Guardian News
+                    </Text>
                 </Box>
+            </Box>
 
-                <TechSummary /> 
+            <Box gridArea="Bullet2">
+                <Box pad={{left: "medium", right: "large"}}>
+                    <Heading level="3" textAlign="start" color="brand">
+                        .Machine Learning: 
+                    </Heading>
+                    <Text color="brand" textAlign="start"> 
+                        -A trained neural network is integrated serverside, based on the powerful Tensorflow API.
+                    </Text>
+                </Box>
+            </Box>
 
-            </Grid>
+            <Box gridArea="Bullet3">
+                <Box pad={{left: "medium", right: "large"}}>
+                    <Heading level="3" textAlign="start" color="brand">
+                        .Supervised Training: 
+                    </Heading>
+                    <Text color="brand" textAlign="start"> 
+                        -The neural network is fully customized and self-trained using 80000+ scrapped tweets.
+                    </Text>
+                </Box>
+            </Box>
+
+            <Box gridArea="Bullet4">
+                <Box pad={{left: "medium", right: "large"}}>
+                    <Heading level="3" textAlign="start" color="brand">
+                        .React Powered 
+                    </Heading>
+                    <Text color="brand" textAlign="start"> 
+                        -The UI is powered by ReactJS and styled on the popular Materialize UI framework.
+                    </Text>
+                </Box>
+            </Box>
+            
+            <Box gridArea="SeeMoreBtn">
+                <Button 
+                    default 
+                    margin="small" 
+                    label="Live Demo" 
+                    href="https://tweetrec.herokuapp.com/"
+                    alignSelf="center"
+                    size="large"
+                />
+            </Box>
+
+            <Box gridArea="TechList" border={{color: "brand", size: "small", style: "solid", side: "left"}}>
+                <Box direction="row">
+                    <Box round={true} border={{color: "brand", size: "small", style: "solid", side: "all"}}>
+                        <Text>
+                            Tech1
+                        </Text>
+                    </Box>
+                    <Box round={true} border={{color: "brand", size: "small", style: "solid", side: "all"}}>
+                        <Text>
+                            Tech2
+                        </Text>
+                    </Box>
+                </Box>
+            </Box>
+        </Grid>
+            
         </Layer>
     )
 
